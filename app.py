48f22f963c603eb299a46633395c87a1147f9add
Raddames Tonui <raddamestonui48@gmail.com>
2025-04-15 20:55:31 +0300

    added postgrs database

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # app.py
-import os
+import os, random
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
+from datetime import timedelta
 
 
 # Load env vars
 load_dotenv()
+postgres_pwd=os.getenv("POSTGRES_PWD")
 
 # Setup extensions
 db = SQLAlchemy()
@@ -19,18 +21,21 @@ def create_app():
     app = Flask(__name__)
 
     # Config
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL' ) postgresql://admin:admin@localhost:5432/sky_survey_db
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://sky_survey_db_9xg7_user:{postgres_pwd}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'supersecretkey')
+    app.config["SECRET_KEY"] = "$hhjd4q%h%^#7&893" + str(random.randint(1, 1000000))
 
     # JWT Configuration for Header-Based Auth
-    app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY', 'your-secret-key')  
-    app.config["JWT_TOKEN_LOCATION"] = ["headers"]  # Change this to headers instead of cookies
+    app.config["JWT_TOKEN_LOCATION"] = ["headers"] 
+    app.config["JWT_SECRET_KEY"] = "a44u5$%*47992n3i*#*#99s29" + str(random.randint(1, 100000))
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+    # print(os.getenv('DATABASE_URL'))
 
     # Init extensions
     db.init_app(app)
     migrate.init_app(app, db)
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173"]) 
+    CORS(app, supports_credentials=True, ) 
 
     jwt = JWTManager(app)
 
