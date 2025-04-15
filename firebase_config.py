1332d954a175af1ddca4ac6facf65cb6eb77073e
Raddames Tonui <raddamestonui48@gmail.com>
2025-04-15 22:05:19 +0300

    added firebase secrets

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials, storage
 import os
+import json
+import base64
+from dotenv import load_dotenv
 
-# Absolute path to the service account
-cred_path = os.path.join(os.path.dirname(__file__), 'secrets', 'jazaform_firebase_secrets.json')
+# Load environment variables
+load_dotenv()
+
+# Load base64 credentials from environment
+firebase_credentials_b64 = os.getenv("FIREBASE_CREDENTIALS_B64")
+if not firebase_credentials_b64:
+    raise ValueError("FIREBASE_CREDENTIALS_B64 is not set in .env")
+
+# Decode and load JSON
+firebase_credentials_json = json.loads(base64.b64decode(firebase_credentials_b64).decode("utf-8"))
 
 # Initialize Firebase app
-cred = credentials.Certificate(cred_path)
+cred = credentials.Certificate(firebase_credentials_json)
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'sheria-365.appspot.com'
 })
